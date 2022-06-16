24ae629a0589f7a00a88ad9341239e9f119603c8
munozcar20 <carlosmz243@gmail.com>
2022-06-15 23:07:35 -0500

    The connection with DB is finished

---
+++ src/main/java/com/example/APIRESTjavaSpringBoot/ApiRestJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.APIRESTjavaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestJavaSpringBootApplication.class, args);
+	}
+
+}
